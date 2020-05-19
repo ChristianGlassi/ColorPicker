f1145cf8df468eac66c6aac6a873ac2c6672b28e
Christian G <ChrisGGlassi@gmail.com>
2020-05-18 20:30:38 -0600

    First Commit

---
+++ build/precache-manifest.c44a88c7f0dad266ed4d542f6862cfce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce35257e0eb53afa5a1d4beea097bf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14f1c32c6755ac3bfb55",
+    "url": "/static/css/main.e4235ec6.chunk.css"
+  },
+  {
+    "revision": "42f378e56f978c67e0b2",
+    "url": "/static/js/2.3d982cb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3d982cb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14f1c32c6755ac3bfb55",
+    "url": "/static/js/main.a83d2b74.chunk.js"
+  },
+  {
+    "revision": "a172eabd7d3933467bed",
+    "url": "/static/js/runtime-main.106dd7c9.js"
+  }
+]);
